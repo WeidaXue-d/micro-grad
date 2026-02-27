b16e52e445cb93aba611d63edbcf8d633d739738
WeidaXue-d <davidxue0607@outlook.com>
2026-02-28 10:49:49 +1100

    Update nn.py

--- micro_autograd/nn.py
+++ micro_autograd/nn.py
@@ -37,11 +37,13 @@ class MLP:
         sz = [nin] + nouts
         self.layers = [Layer(sz[i],sz[i+1]) for i in range(len(nouts))]
 
+
     def __call__(self,x):
         for layer in self.layers:
             x = layer(x)
         return x
     
+
     def parameters(self):
         return [p for layer in self.layers for p in layer.parameters()]
 
